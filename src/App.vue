ae566f358d80706a6bbf06ee4ddf1744039ad602
ken-teaches-web <kkorth@mcad.edu>
2022-03-21 20:27:57 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<script setup>
+import CamelidComponent from "./components/CamelidComponent.vue";
+// javascript
+const camelids = [
+  {
+    commonName: "Lama",
+    binomialName: "Lama glama",
+    order: "Artiodactyla",
+    family: "Camelidae",
+    genus: "Lama",
+    species: "L. glama",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Llama_lying_down.jpg/220px-Llama_lying_down.jpg",
+  },
+  {
+    commonName: "Alpaca",
+    binomialName: "Vicugna pacos",
+    order: "Artiodactyla",
+    family: "Camelidae",
+    genus: "Vicugna",
+    species: "V. pacos",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Alpaca_%2831562329701%29.jpg/440px-Alpaca_%2831562329701%29.jpg",
+  },
+];
+</script>
+
+<template>
+  <!-- html -->
+  <h1>My favorite Camelids</h1>
+  <div class="camelids-wrapper">
+    <CamelidComponent
+      v-for="animal in camelids"
+      v-bind:key="animal.commonName"
+      v-bind:animal="animal"
+    />
+  </div>
+</template>
+
+<style>
+/* css */
+h1 {
+  text-align: center;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+  font-size: 64px;
+}
+.camelids-wrapper {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+</style>
---
+++ src/components/CamelidComponent.vue
@@ -0,0 +1,27 @@
+<script setup>
+const props = defineProps(["animal"]);
+</script>
+<template>
+  <div class="card">
+    <h3>{{ props.animal.commonName }}</h3>
+    <small>binomial name: {{ props.animal.binomialName }}</small>
+    <p>order: {{ props.animal.order }}</p>
+    <p>family: {{ props.animal.family }}</p>
+    <p>genus: {{ props.animal.genus }}</p>
+    <p>species {{ props.animal.species }}</p>
+    <img v-bind:src="props.animal.image" v-bind:alt="props.animal.commonName" />
+  </div>
+</template>
+<style scoped>
+.card {
+  border: 1px solid #333;
+  border-radius: 4px;
+  width: 220px;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+}
+img {
+  max-width: 200px;
+}
+</style>
